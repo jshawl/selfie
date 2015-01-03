011e0cd998e45d0a379ab623b5d086cab2342f03
Jesse Shawl <jesse@jshawl.com>
2015-01-03 14:14:42 -0500

    add sinatra

--- app.rb
+++ app.rb
@@ -1,7 +1,11 @@
 require 'pry'
+require 'sinatra'
+require 'sinatra/reloader'
 require 'active_record'
 
 require_relative 'db/connection'
 require_relative 'models/selfie'
 
-binding.pry
+get '/' do
+  "hello world"
+end
